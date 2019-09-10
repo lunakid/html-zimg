79d554d97eac745719d131bc6df2fd8da8fd3399
lunakid <lunakid@gmail.com>
2019-09-11 00:15:31 +0200

    Unified DOM. Fixed off-by-one magn. sign pos. Docs started.

--- README.txt
+++ README.txt
@@ -1,1 +1,40 @@
 The example image is one of Édouard Martinet's amazing metallic animal sculptures.
+
+
+Supported modes
+---------------
+
+Basic in-place:
+
+	The simplest, and least useful mode, where the magnified image simply replaces
+	the thumbnail view, and is part of the normal content flow, therefore it will
+	also change the page layout on zooming, pushing content away to make room.
+
+	The extra thumbnail image in the DOM is a dummy, unused, invisible element,
+	solely to support a uniform DOM structure.
+	
+
+In-place:
+
+	Unlike the basic in-place mode, the magnified view that replaces the thumbnail
+	on hover will *not* change the page layout (essentially acting like a popup overlay).
+
+	The thumbnail img is only needed for sizing the widget (as the thumbnail),
+	because the zoom overlay element is `absolute`-positioned, taking up no space.
+	
+	The thumbnail img. will be overlapped by the zoom view both while zooming/panning,
+	and also when idle, providing the (reset) thumbnail view.
+	
+	The full-size img. in the `FIGURE` element will guide the aspect-ratio when
+	resizing.
+
+Popup:
+
+	The zoom view is a "popup" overlay that is *distinct* from the thumbnaiil,
+	where all the panning occurs. (So, the event handler is attached to the thumb,
+	not the zoom view.)
+
+	The full-size image embedded in the `FIGURE` element is always hidden:
+	the background image will be used for zooming/panning instead.
+	It only exists to guide the aspect-ratio when resizing the `FIGURE`.
+
--- zoomer-test.php
+++ zoomer-test.php
@@ -1,53 +1,30 @@
 <?php
+function DOM($mode, $img_url)
+{
+	$ieh = ($mode == 'popup' ? " onmousemove='popup_zoom(event)'" : "");
+	$feh = ($mode == 'popup' ? "" : " onmousemove='zoom(event)'");
+	return ""
+		. "<div class='zoomable $mode'>"
+		. "<img class='thumb' src='$img_url'" . $ieh . ">"
+		. "<figure class='zoom' style='background-image: url($img_url)'" . $feh . ">"
+		.	"<img src='$img_url'>"
+		. "</figure>"
+		. "<div class='mag'></div>"
+		. "</div>" // no '\n', as that would be rendered as visible spacing!
+	;
+}
 
 function zoomable_img($img_url, $mode = 'popup')
 {
 	static $onceonly_stuff = false;
-	
 	if (!$onceonly_stuff) {
 	     $onceonly_stuff = true;
 	     print @file_get_contents("zoomer.html");
 	}
 
-	switch ($mode)
-	{
-	case 'inplace':
-		print ""
-			. "<div class='zoomable $mode'>"
-			// This invisible image is only needed for dimensioning the thumbnail:
-			. "<img class='thumb' src='$img_url'>" //! overlapped by `FIGURE` while zooming/panning
-			. "<figure class='zoom' style='background-image: url($img_url)' onmousemove='zoom(event)'>"
-				// This img. guides the aspect-ratio when resizing `FIGURE`, and provides the idle view:
-			.	"<img src='$img_url'>"
-			. "<div class='mag'></div>"
-			. "</figure>"
-			. "</div>"
-		;
-		break;
-	case 'popup':
-		print ""
-			. "<div class='zoomable $mode'>"
-			. "<img class='thumb' src='$img_url' onmousemove='popup_zoom(event)'>"
-			. "<figure class='zoom' style='background-image: url($img_url)'>"
-				// This invisible img only exists to guide the aspect-ratio when resizing the FIGURE:
-			.	"<img src='$img_url'>"
-			. "</figure>"
-			. "<div class='mag'></div>"
-			. "</div>"
-		;
-		break;
-	case 'basic':
-		print ""
-			. "<div class='zoomable $mode'>"
-			. "<figure class='zoom' style='background-image: url($img_url)' onmousemove='zoom(event)'>"
-			.	"<img src='$img_url'>"
-			. "<div class='mag'></div>"
-			. "</figure>"
-			. "</div>"
-		;
-		break;
-	}
+	print DOM($mode, $img_url);
 }
+
 
 $img = "example.jpg";
 zoomable_img($img, 'inplace');
